c7ac30e88347c48aaff6914e9d36389907d3a8c6
Tejasree Bayanagari <t.bayanagari@zensar.com>
2022-04-23 12:55:01 +0530

    c#Assignment

---
+++ Assignment2/Assignment2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2
+{
+    internal class Program
+    {
+        static void Main(String[] args)
+        {
+            //To assign integer to an array -- Average value , maximum number ,minimum number
+            int n;
+            Console.WriteLine("Enter array size ");
+            n = Convert.ToInt32(Console.ReadLine());
+            int[] array = new int [n];
+            Console.WriteLine("Enter array values: ");
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Enter  array value of : " + i);
+                array[i]= Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("Array elements are : ");
+            for (int i = 0; i < n; i++)
+            
+            {
+                Console.WriteLine(array [i]);
+            }
+            Console.WriteLine("Average of the given is " + array.Average());
+            Console.WriteLine("Maximum number in array is " + array .Max());
+            Console.WriteLine("Minimum number in array is" + array.Min());
+            Console.ReadLine();
+
+
+
+
+        }
+    }
+}
