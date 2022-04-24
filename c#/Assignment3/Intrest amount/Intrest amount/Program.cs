f420df2ecc9cb177a792b5f1969a29956851a3fd
Tejasree Bayanagari <t.bayanagari@zensar.com>
2022-04-24 10:29:36 +0530

    c# Assignments

---
+++ c#/Assignment3/Intrest amount/Intrest amount/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intrest_amount
+{
+    internal class Intrest_amount
+    {
+        double rate;
+       main ()
+       public void loanCalculator(double loanamount)
+        {
+            Console.WriteLine("Enter rate of intrest");
+            rate = Convert.ToDouble(Console.ReadLine());
+            double intrest = (loanamount * rate) / 100;
+            Console.WriteLine("Rate of intrest for " + rate + "% is :" + intrest);
+        }
+    }
+}
---
+++ c#/Assignment3/Student/Student/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            Console.WriteLine("Enter student name : ");
+            String name = Console .ReadLine();   
+            Console.WriteLine("enter student roll no:");
+            double rollno = Convert.ToDouble(Console.ReadLine());   
+            Console.WriteLine("Enter student class ");
+            double stclass = Convert.ToDouble(Console.ReadLine()); 
+            Console.WriteLine(" Enter Student semester");
+            double semester = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter Student branch");
+            String branch = Console.ReadLine();
+            int[] marks= new int[i];
+            Console.WriteLine(" Enter marks ");
+            Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < marks.Length; i++)
+            {
+                Console.WriteLine(marks[i]);
+            }
+            double average =marks.Average();
+            Console.WriteLine(average);
+            if(marks[i] < 35)
+            {
+                Console.WriteLine("Student fail");
+
+            }
+            else
+            {
+                Console.WriteLine("Student pass");
+            }
+            {
+                Console.WriteLine("$ rollno is{rollno},name is{name},stclass is{class},semeser is {semester} , branch is {branch}");
+            }
+            {
+                Console.WriteLine("The result is ");
+            }          
+        }
+    }
+}
