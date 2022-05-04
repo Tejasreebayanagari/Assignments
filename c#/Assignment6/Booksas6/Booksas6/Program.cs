67734bd044e009025e405d074edc4bb90c7db86e
Tejasree Bayanagari <t.bayanagari@zensar.com>
2022-05-04 22:50:48 +0530

    C# Assignment6

---
+++ c#/Assignment6/Booksas6/Booksas6/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Booksas6
+{
+    class Books
+    {
+        public string Name;
+        private int Book_Id { get; set; }
+
+        public int Book_ID
+        {
+            get { return Book_Id; }
+            set { Book_Id = value; }
+        }
+        public void Display()
+        {
+            Console.WriteLine("Book id is:{0}", Book_Id);
+            Console.WriteLine($"\nBook name is:{Name}");
+        }
+
+
+    }
+    class Structbook
+    {
+        static void Main()
+        {
+            Books b = new Books();
+            b.Book_ID = 01;
+            b.Name = "Pride of indian architecture";
+            b.Display();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ c#/Assignment6/Studentas6/Assignment6/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_6
+{
+    interface IStudent
+    {
+        void ShowDetails(int id, string name);
+    }
+    class DaySholar : IStudent
+    {
+        public void ShowDetails(int id, string name)
+        {
+            Console.WriteLine("Student Id is:{0}\n\nStudent name is:{1}", id, name);
+        }
+    }
+    class Resident : IStudent
+    {
+        public void ShowDetails(int id, string name)
+        {
+            Console.WriteLine("Student id is:{0}\n\nStudent name is:{1}", id, name);
+        }
+    }
+    class InterfaeEg
+    {
+        static void Main()
+        {
+            Console.WriteLine("Dayscholar Details");
+            IStudent d = new DaySholar();
+            d.ShowDetails(1, "Teju");
+
+            Console.WriteLine("\nResident Scholar Details");
+            IStudent r = new Resident();
+            r.ShowDetails(2, "Teju");
+            Console.ReadLine();
+        }
+    }
+}
