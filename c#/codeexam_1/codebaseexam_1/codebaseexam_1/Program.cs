6e32b37663ef032bdaaaeb10e049b522aa4d81bb
Tejasree Bayanagari <t.bayanagari@zensar.com>
2022-05-05 10:10:35 +0530

    c# codeexam_1

--- c#/codeexam_1/codebaseexam_1/codebaseexam_1/Program.cs
+++ c#/codeexam_1/codebaseexam_1/codebaseexam_1/Program.cs
@@ -8,8 +8,46 @@ namespace codebaseexam_1
 {
     internal class Program
     {
-        static void Main(string[] args)
+        class Passenger
         {
+            string name;
+            string DateofTravel;
+            int Age;
+            Passenger(string name, string DateofTravel, int Age)
+            {
+                this.name = name;
+                this.DateofTravel = DateofTravel;
+                this.Age = Age;
+            }
+
+            public void TicketBooking(int no_of_tickets)
+            {
+                if (no_of_tickets > 5)
+                {
+                    throw (new TicketsException("No more than 5 tickets"));
+                }
+                else
+                {
+                    Console.WriteLine("Tickets successfully: {0}", no_of_tickets);
+
+                }
+            }
+
+            public class TicketsException : Exception
+            {
+                public TicketsException(string message) : base(message)
+                {
+                }
+            }
+
+            public static void Main()
+            {
+                int no_tickets;
+                no_tickets = Convert.ToInt32(Console.Read());
+                Console.WriteLine(no_tickets);
+                Passenger pass = new Passenger("Tejasree", "5-05-2022", 23);
+                pass.TicketBooking(no_tickets);
+            }
         }
     }
 }
