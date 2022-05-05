fce8b37097a0d681f5b3e79fb3750af637d54610
Tejasree Bayanagari <t.bayanagari@zensar.com>
2022-05-05 21:44:34 +0530

    c# assignment5

---
+++ c#/codeexam_1/day13/day13/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+struct Sample
+{
+    public int i;
+}
+class MyProgram
+{
+    static void Main()
+    {
+        Sample x = new Sample();
+        x.i = 10;
+        fun(x);
+        Console.Write(x.i + "" );
+    }
+    static void fun(Sample y)
+    {
+        y.i = 20;
+        Console.Write(y.i + "" );
+        Console.ReadKey();
+    }
+}
+    
