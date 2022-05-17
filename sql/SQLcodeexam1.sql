df22aa57075cc853040cf5f405786cbc46006300
Tejasree Bayanagari <t.bayanagari@zensar.com>
2022-05-17 18:35:27 +0530

    sql sqlcodeexam1

---
+++ sql/SQLcodeexam1.sql
@@ -0,0 +1,11 @@
+create database sqlexam1
+use sqlexam1
+create  table employees
+(
+empid int not null,
+name varchar (222),
+salary int ,
+deptno int identity(10,10)
+constrant chk_sal check (salary >= 5000 and salary <=50000)
+)
+select * from employees
