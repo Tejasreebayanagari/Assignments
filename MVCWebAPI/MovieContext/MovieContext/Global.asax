c9f95616098f5b4061d1f8d6d759e4aa7405f218
Tejasree Bayanagari <t.bayanagari@zensar.com>
2022-06-11 16:07:56 +0530

    MVC_Movies

---
+++ MVCWebAPI/MovieContext/MovieContext/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MovieContext.MvcApplication" Language="C#" %>
---
+++ MVCWebAPI/MovieContext/MovieContext/Models/Movie.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace MovieContext.Models
+{
+
+    public class Movie_Context : DbContext
+    {
+        public Movie_Context() : base("Name=MovieEntites")
+        {
+
+        }
+
+        public DbSet<Movie_Context> Movies { get; set; }
+    }
+}
