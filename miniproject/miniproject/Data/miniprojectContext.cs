440b95669e47aeee2c90a8494d1f0816929d2256
Tejasree Bayanagari <t.bayanagari@zensar.com>
2022-07-09 16:31:35 +0530

    miniproject

---
+++ miniproject/miniproject/Data/miniprojectContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using miniproject.Models;
+
+namespace miniproject.Data
+{
+    public class miniprojectContext : DbContext
+    {
+        public miniprojectContext (DbContextOptions<miniprojectContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<miniproject.Models.Products>? Products { get; set; }
+    }
+}
---
+++ miniproject/miniproject/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace miniproject.Models
+{
+    
+        public class Products
+        {
+            [Key]
+            public int Id { get; set; }
+            [Required]
+            public string? ProductName { get; set; }
+            public int Price { get; set; }
+            public string? brand { get; set; }
+            public string? ManufactureDate { get; set; }
+            public string? ExpirationDate { get; set; }
+        }
+    
+}
